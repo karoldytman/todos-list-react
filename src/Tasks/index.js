bad7231461d7f5112cd33fa0dd791242882b1c89
Karol Dytman <dytmankarol@wp.pl>
2023-04-03 17:53:09 +0200

    restructuring

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,9 +1,9 @@
 import "./style.css";
-const Tasks = (props) => ( 
+const Tasks = ({ tasks, hideDoneTasks }) => ( 
 <ul className = "tasks"> {
-        props.tasks.map(task => ( 
+        tasks.map(task => ( 
         <li key = {task.id}
-            className = {`tasks__item ${task.done && props.hideDoneTasks === true ? "tasks__item--hiden" : ""}`} >
+            className = {`tasks__item ${task.done && hideDoneTasks === true ? "tasks__item--hiden" : ""}`} >
             <button className ="tasks__button tasks__button--done" > {task.done ? "✔" : ""} </button>
             <span className = {`tasks__Content ${task.done ? "task__content--done" : ""}`} > {task.content}</span>
             <button className ="tasks__button tasks__button--remove" >🗑</button>
@@ -11,8 +11,7 @@ const Tasks = (props) => (
         
 
             </li>
-        ))
-    }</ul>
+        ))}</ul>
 );
 
 export default Tasks;
