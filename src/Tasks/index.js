086ccf574967c3dbeb5aa3cc29c785c55a668b4e
Karol Dytman <dytmankarol@wp.pl>
2023-04-04 18:21:25 +0200

    adding removeTask

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,14 +1,13 @@
+import React from "react";
 import "./style.css";
-const Tasks = ({ tasks, hideDoneTasks, remuveTask }) => ( 
-<ul className = "tasks"> {
-        tasks.map(task => ( 
-        <li key = {task.id}
-            className = {`tasks__item ${task.done && hideDoneTasks === true ? "tasks__item--hiden" : ""}`} >
-            <button className ="tasks__button tasks__button--done" > {task.done ? "✔" : ""} </button>
-            <span className = {`tasks__Content ${task.done ? "task__content--done" : ""}`} > {task.content}</span>
-            <button 
-            onClick={() => remuveTask(task.id)}
-            className ="tasks__button tasks__button--remove" >🗑</button>
+const Tasks = ({ tasks, hideDoneTask, removeTask }) => ( 
+<ul className = "tasks"> 
+        {tasks.map(task => ( 
+        <li key ={task.id}
+            className ={`tasks__item${task.done && hideDoneTask === true ? "tasks__item--hiden" : ""}`}>
+            <button className ="tasks__button tasks__button--done">{task.done ? "✔" : ""}</button>
+            <span className ={`tasks__Content${task.done ? "task__content--done" : ""}`}>{task.content}</span>
+            <button className ="tasks__button tasks__button--remove" onClick={() => removeTask(task.id)}>🗑</button>
 
         
 
