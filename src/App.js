3ff693cabeb97bdd63926eae9c75cda950d2d8fd
Karol Dytman <dytmankarol@wp.pl>
2023-04-04 18:20:04 +0200

    restruktusisation js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+
 function App() {
   const [hideDoneTask, setHideDoneTask] = useState(false);
   const [tasks, setTasks] = useState([
-    {id: 1, content: "Zjeść obiad", done: false},
-    {id: 2, content: "Umyć naczycia", done: true},
+    { id: 1, content: "Zjeść obiad", done: false },
+    { id: 2, content: "Umyć naczycia", done: true },
 ]);
   const toggleHideDoneTask = () => {
     setHideDoneTask(hideDoneTask => !hideDoneTask);
@@ -17,12 +18,12 @@ function App() {
     const removeTask = (id) =>{
       setTasks(tasks => tasks.filter(task => task.id !== id));
     };
-  
+
   return (
     <Container>
    <Header title="Lista zadań" />
     <Section title="Dodaj nowe zadanie" body={<Form />} />
-   <Section title="Lista zadań" body={ <Tasks tasks={tasks} hideDoneTask={hideDoneTask} removeTask={removeTask} />}
+   <Section title="Lista zadań" body={<Tasks tasks={tasks} hideDoneTask={hideDoneTask} removeTask={removeTask} />}
    ExContent={<Buttons 
     tasks={tasks} hideDoneTask={hideDoneTask} toggleHideDoneTask={toggleHideDoneTask} />} 
    />
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,21 +1,21 @@
+import React from "react";
 import "./style.css"
-const Buttons = ( {tasks, hideDoneTasks, toggleHideDoneTask} ) => {
-if(tasks.length === 0) {
-    return null
-}
-return(
+const Buttons = ({ tasks, hideDoneTask, toggleHideDoneTask }) => (
     <div className="buttons">
+        {tasks.length > 0 && (
+            <>
            <button onClick={toggleHideDoneTask} className="buttons__button hideShowButton">
-    {hideDoneTasks === false ? "Pokaż" : "Ukryj"} ukończone
+    {hideDoneTask === false ? "Pokaż" : "Ukryj"} ukończone
     </button>
     <button className="buttons__button"
     disabled={tasks.every(({ done }) => done)}
     >
     Ukończ wszystkie
     </button>
+    </>
+        )}
     </div>
- 
     );
-}
+
 
 export default Buttons;
