2f0ed04f3f92377ffb2a76570a0ac4735a53f801
Karol Dytman <dytmankarol@wp.pl>
2023-04-06 16:34:50 +0200

    setAllDone interaction

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -4,16 +4,16 @@ const Buttons = ({ tasks, hideDoneTask, toggleHideDoneTask, setAllDone }) => (
     <div className="buttons">
         {tasks.length > 0 && (
             <>
-           <button onClick={toggleHideDoneTask} className="buttons__button hideShowButton">
+           <button onClick={toggleHideDoneTask} className="buttons__button">
     {hideDoneTask ? "Pokaż" : "Ukryj"} ukończone
     </button>
-    <button onClick={() => setAllDone} className="buttons__button"
+    <button onClick={setAllDone} className="buttons__button"
     disabled={tasks.every(({ done }) => done)}
     >
     Ukończ wszystkie
     </button>
     </>
-        )};
+        )}
     </div>
     );
 
